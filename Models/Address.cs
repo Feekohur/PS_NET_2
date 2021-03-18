0b2c6f86231456056e1ed4c948be24fe78a59b7d
Michał Król <77741@student.pb.edu.pl>
2021-03-18 11:32:29 +0100

    Wyczyszczony git ignore

---
+++ Models/Address.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PS_NET_2.Models
+{
+    public class Address
+    {
+        [Display(Name = "Twoja ulubiona ulica")]
+        [Required(ErrorMessage ="Podaj ulice")]
+
+        public string Street { get; set; }
+        [Display(Name = "Zipcode")]
+        [StringLength(60,MinimumLength = 3), Required]
+        public string ZipCode { get; set; }
+        [Display(Name = "City")]
+        [Required(ErrorMessage = "Podaj miasto")]
+        public string City { get; set; }
+        [Display(Name = "Number")]
+        [Required]
+        public int Number { get; set; }
+    }
+}
